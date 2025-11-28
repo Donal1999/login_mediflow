eba48d7a81638764f1f1739e87d198d52b551f18
DONALOMAOILCHIARAIN- <g00365724@atu.ie>
2025-11-28 10:43:49 +0000

    added to entity class

--- src/main/java/com/example/login_mediflow/User.java
+++ src/main/java/com/example/login_mediflow/User.java
@@ -3,16 +3,25 @@ package com.example.login_mediflow;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@Table(name = "users")
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
     @Id
     @GeneratedValue
-    private Long id;
+    @NotBlank @Size(min = 1, max = 20)
+    private String userName;
+    @NotBlank @Size(min = 1, max = 20)
+    private String password;
+    @NotBlank @Size(min = 1, max = 20)
+    private String role;
 }
---
+++ src/main/java/com/example/login_mediflow/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.login_mediflow;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByUserName(String userName);
+}
