ca0495e7d77682069f0e25e3d8098ba887d5a417
Donal1999 <g00365724@atu.ie>
2025-11-29 19:35:37 +0000

    added MethodArgumentNotValidException handler

---
+++ src/main/java/com/example/login_mediflow/ExecptionDetails.java
@@ -0,0 +1,27 @@
+package com.example.login_mediflow;
+
+public class ExecptionDetails {
+    private String fieldName;
+    private String fieldValue;
+
+    public ExecptionDetails(String fieldName, String fieldValue) {
+        this.fieldName = fieldName;
+        this.fieldValue = fieldValue;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public void setFieldName(String fieldName) {
+        this.fieldName = fieldName;
+    }
+
+    public String getFieldValue() {
+        return fieldValue;
+    }
+
+    public void setFieldValue(String fieldValue) {
+        this.fieldValue = fieldValue;
+    }
+}
